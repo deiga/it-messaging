e211c82e5a819b37c4f8face9accac70dfc385f5
Timo Sand <timo.j.sand@gmail.com>
2013-01-03 16:22:14 +0200

    CHANGED: Connection to open fire established

--- it_messaging.rb
+++ it_messaging.rb
@@ -1,7 +1,18 @@
+require 'xmpp4r'
+include Jabber
+
 class ItMessaging < Sinatra::Base
 
+  configure :development do
+    require 'sinatra/reloader'
+    register Sinatra::Reloader
+    Jabber::debug = true
+  end
+
   get '/' do
-    "Hello World"
+    client = Client.new(JID::new("test@guinevere.local/it-messaging"))
+    client.connect
+    client.auth("tester")
   end
 
 end
