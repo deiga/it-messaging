b6f71a0cab955bbca29cc8147e8a9a35587e5506
Timo Sand <timo.j.sand@gmail.com>
2013-01-06 14:57:40 +0200

    CHANGED: Added ability to send messages

--- it_messaging.rb
+++ it_messaging.rb
@@ -9,29 +9,35 @@ class ItMessaging < Sinatra::Base
     Jabber::debug = true
   end
 
-  @@jid = JID::new("test","guinevere.local","itm")
-  @@xmpp = Client.new(@@jid)
-  @@xmpp.connect
-  @@xmpp.auth("tester")
+  configure do
+    @@jid = JID::new("test","guinevere.local","itm")
+    @@xmpp = Client.new(@@jid)
+    @@xmpp.connect
+    @@xmpp.auth("tester")
 
-  @@messages_times = []
+    @@messages_times = []
 
-  @@xmpp.add_message_callback do |m|
-    delay_elem = m.first_element('delay')
-    tstamp = delay_elem.attribute('stamp').to_s unless delay_elem.nil?
-    time = (tstamp.nil? || tstamp.empty?) ? Time.new : Time.parse(tstamp)
-    @@messages_times << {message: m, time: time.to_time}
-    # p "DBG: #{m}"
-    # p "DBG: #{@@messages_times.length}"
+    @@xmpp.add_message_callback do |m|
+      delay_elem = m.first_element('delay')
+      tstamp = delay_elem.attribute('stamp').to_s unless delay_elem.nil?
+      time = (tstamp.nil? || tstamp.empty?) ? Time.new : Time.parse(tstamp)
+      @@messages_times << {message: m, time: time.to_time}
+    end
+    @@xmpp.send(Presence.new.set_type(:available))
+    sleep 5
   end
-  @@xmpp.send(Presence.new.set_type(:available))
-  sleep 5
 
 
   get '/' do
     @messages_times = @@messages_times
-    # p "LOG: #{@messages_times.inspect}"
     haml :index
+  end
+
+  post '/send' do
+    m = Message.new(JID::new(params[:nick], 'guinevere.local'), params[:message])
+    @@xmpp.send(m)
+
+    redirect to '/'
   end
 
 end
